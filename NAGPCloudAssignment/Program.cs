42b49917cb14920bd3d891117a7181b07f2fbb73
rajat1883 <32308062+rajat1883@users.noreply.github.com>
2019-06-02 21:52:03 +0200

    Update Program.cs

--- NAGPCloudAssignment/Program.cs
+++ NAGPCloudAssignment/Program.cs
@@ -19,6 +19,8 @@ namespace NAGPCloudAssignment
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseUrls("http://*:8001")
+                .UseStartup<Startup>()
+                .Build();
     }
 }
