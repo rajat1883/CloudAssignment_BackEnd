6d07d3503eeaaff831a1e6c3af8d9237d2fb308b
rajat1883 <32308062+rajat1883@users.noreply.github.com>
2019-06-02 20:54:00 +0200

    Adding CORS

--- NAGPCloudAssignment/Startup.cs
+++ NAGPCloudAssignment/Startup.cs
@@ -24,12 +24,19 @@ namespace NAGPCloudAssignment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
